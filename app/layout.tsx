7bed4e1bbf84c7dc1d0315543cc4a07d8b2b556a
Mohammad Ghufran <mohammadghufran@Mohammads-MacBook-Air.local>
2026-06-07 19:25:12 +0530

    feat: Add Google site verification metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,9 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
+  },
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
   }
 };
 
